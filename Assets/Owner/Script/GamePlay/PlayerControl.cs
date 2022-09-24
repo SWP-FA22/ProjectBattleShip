f0292b35b680a238f7cc2bab559ae96f4f3a2255
khai1412 <khai14122002123@gmail.com>
2022-09-24 09:21:14 +0700

    change

--- Assets/Owner/Script/GamePlay/PlayerControl.cs
+++ Assets/Owner/Script/GamePlay/PlayerControl.cs
@@ -7,34 +7,46 @@ public class PlayerControl : MonoBehaviour
 {
     public PhotonView view;
     public GameObject camera;
-    public float      speed;
+    public float speed;
+
+    public GameObject canon;
     // Start is called before the first frame update
     void Start()
     {
         view = gameObject.GetComponent<PhotonView>();
         this.camera = GameObject.Find("CM vcam1");
     }
-    
+
     void Update()
     {
-        if(view.IsMine)
+        if (view.IsMine)
         {
             this.camera.GetComponent<CinemachineVirtualCamera>().Follow = gameObject.transform;
             this.camera.GetComponent<CinemachineVirtualCamera>().LookAt = gameObject.transform;
-            if(Input.GetKey(KeyCode.W)){
-                gameObject.transform.position += transform.right * Mathf.Clamp01(1) * this.speed*Time.deltaTime;
+            if (Input.GetKey(KeyCode.W))
+            {
+                gameObject.transform.position += transform.right * Mathf.Clamp01(1) * this.speed * Time.deltaTime;
             }
-            if(Input.GetKey(KeyCode.A)){
-                float rotation = 1 * this.speed*0.1f;
-                transform.Rotate(Vector3.forward*rotation);
+            if (Input.GetKey(KeyCode.A))
+            {
+                float rotation = 1 * this.speed * 0.1f;
+                transform.Rotate(Vector3.forward * rotation);
             }
-            if(Input.GetKey(KeyCode.D)){
-                float rotation = -1 * this.speed*0.1f;
-                transform.Rotate(Vector3.forward*rotation);
+            if (Input.GetKey(KeyCode.D))
+            {
+                float rotation = -1 * this.speed * 0.1f;
+                transform.Rotate(Vector3.forward * rotation);
             }
-            
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            canon.transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - canon.transform.position);
+
         }
     }
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
 
-    
+
 }
